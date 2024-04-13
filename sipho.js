b63018f09ca89e8611d0209fd3c88ccd797ed7cd
siphokuhlenyana <siphokuhlenyana@gmail.com>
2024-04-13 12:29:34 +0200

    Edited the textfile

---
+++ sipho.js
@@ -0,0 +1,33 @@
+/*This is a comment single line comment
+ghbiknsd*/
+//declare a variable that is an interger 
+
+let age =20
+const x =40
+var deci=20.5
+
+//declare String 
+let firstName="Sipho"//double quotes
+let name='Sipho'//single quotes
+let sentence=`My name is Sipho and I am ${age} years old `//template literals
+//concatenate //concatenation
+let newSentence='My name is Sipho and I am '+ age+'  years old '
+
+//() paranthesis
+//[]brackets
+//{}braces
+//boolean
+let answer =true
+let answer2=false
+
+//declare NULL
+let anynameforvariable=null
+//declare Object 
+let obj={
+    firstName :'Sipho' ,
+    surname :'Nyana',
+    height :'1.72'
+}
+
+// Array --indexing 
+let array=['Sipho',true,20.5,'Nyana']
